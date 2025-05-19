02de76bdf673820db214e68826ee08e385fb77ca
ldporsius <linda.d.porsius@gmail.com>
2025-05-19 15:46:18 +0200

    pens premium

--- app/src/main/java/nl/codingwithlinda/scribbledash/core/data/shop/product_manager/PenManager.kt
+++ app/src/main/java/nl/codingwithlinda/scribbledash/core/data/shop/product_manager/PenManager.kt
@@ -1,13 +1,19 @@
 package nl.codingwithlinda.scribbledash.core.data.shop.product_manager
 
+import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenCopperAura
+import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenCoralReef
 import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenCrimsonRed
 import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenEmeraldGreen
 import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenFlameOrange
+import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenGoldenGlow
+import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenMajesticIndigo
 import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenMidnightBlack
 import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenOceanBlue
 import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenRainbow
 import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenRoseQuartz
+import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenRoyalPurple
 import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenSunshineYellow
+import nl.codingwithlinda.scribbledash.core.data.shop.products.pens.PenTealDream
 import nl.codingwithlinda.scribbledash.core.domain.model.shop.products.PenProduct
 
 object PenManager {
@@ -25,7 +31,13 @@ object PenManager {
     )
 
     val pensPremiumTier = listOf(
-        PenRoseQuartz()
+        PenRoseQuartz(),
+        PenRoyalPurple(),
+        PenTealDream(),
+        PenGoldenGlow(),
+        PenCoralReef(),
+        PenMajesticIndigo(),
+        PenCopperAura()
     )
 
     val pensLegendaryTier: List<PenProduct> = listOf(
---
+++ app/src/main/java/nl/codingwithlinda/scribbledash/core/data/shop/products/pens/PenMajesticIndigo.kt
@@ -0,0 +1,22 @@
+package nl.codingwithlinda.scribbledash.core.data.shop.products.pens
+
+import androidx.compose.ui.graphics.toArgb
+import nl.codingwithlinda.scribbledash.core.domain.model.shop.products.BasicPenProduct
+import nl.codingwithlinda.scribbledash.core.domain.model.shop.products.PenProduct
+import nl.codingwithlinda.scribbledash.core.presentation.design_system.theme.MajesticIndigo
+import nl.codingwithlinda.scribbledash.core.presentation.design_system.theme.RoseQuartz
+import nl.codingwithlinda.scribbledash.core.presentation.design_system.theme.RoyalPurple
+import nl.codingwithlinda.scribbledash.core.presentation.design_system.theme.SunshineYellow
+import nl.codingwithlinda.scribbledash.core.presentation.design_system.theme.TealDream
+import nl.codingwithlinda.scribbledash.core.presentation.util.UiText
+
+class PenMajesticIndigo(): BasicPenProduct() {
+    override val color: Int
+         = MajesticIndigo.toArgb()
+    override val id: String
+        = this::class.java.simpleName
+    override val title: UiText
+        = UiText.DynamicText("MajesticIndigo")
+    override val price: Int
+        = 0
+}

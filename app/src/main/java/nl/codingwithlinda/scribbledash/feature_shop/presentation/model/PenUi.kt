324cb45ee455ba217c2324443714132d72d4bb7d
ldporsius <linda.d.porsius@gmail.com>
2025-05-19 16:47:32 +0200

    pens legendary rainbow in shop

--- app/src/main/java/nl/codingwithlinda/scribbledash/feature_shop/presentation/model/PenUi.kt
+++ app/src/main/java/nl/codingwithlinda/scribbledash/feature_shop/presentation/model/PenUi.kt
@@ -2,20 +2,34 @@ package nl.codingwithlinda.scribbledash.feature_shop.presentation.model
 
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.graphics.Matrix
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import nl.codingwithlinda.scribbledash.core.data.draw_examples.util.pathToCoordinates
+import nl.codingwithlinda.scribbledash.core.data.draw_examples.util.resourceToDrawPaths
 import nl.codingwithlinda.scribbledash.core.domain.model.shop.products.BasicPenProduct
 import nl.codingwithlinda.scribbledash.core.domain.model.shop.products.MultiColorPenProduct
 import nl.codingwithlinda.scribbledash.core.domain.model.shop.products.PenProduct
+import nl.codingwithlinda.scribbledash.core.presentation.design_system.theme.RainbowPenBrush
+import nl.codingwithlinda.scribbledash.core.presentation.design_system.theme.rainbowColors
+import nl.codingwithlinda.scribbledash.feature_game.draw.data.path_drawers.MultiColorPathCreator
 
 @Composable
 fun PenProduct.toUi(
@@ -39,16 +53,47 @@ fun PenProduct.toUi(
 
     if (this is MultiColorPenProduct){
         val centralImage = this.imageResourceId
+        val context = LocalContext.current
         val resources = LocalContext.current.resources
 
-        Box(modifier = modifier) {
-            Image(
-                painter = painterResource(id = centralImage),
-                contentDescription = "scribble",
-                modifier = Modifier.fillMaxSize().padding(4.dp),
-                contentScale = ContentScale.FillWidth
+        val colors = this.colors.map {
+            Color(it)
+        }
 
+        val paths = resourceToDrawPaths(centralImage, context).map {
+            pathToCoordinates(it, 1f)
+        }.flatten()
+            .map {
+                Offset(it.x, it.y)
+            }
+
+        val coloredPaths = MultiColorPathCreator(colors).drawPath(paths).paths
+
+
+        coloredPaths.fastForEach{
+            it.path.asComposePath().transform(
+                Matrix(
+
+                ).apply {
+                   this.scale(3.5f, 4f)
+                }
             )
+        }
+        Box(modifier = modifier) {
+            androidx.compose.foundation.Canvas(
+                modifier = Modifier.fillMaxSize()
+                    .padding(4.dp)
+            ) {
+                coloredPaths.forEachIndexed { index, path ->
+                    drawPath(
+                        path = path.path.asComposePath(),
+                        color = path.color,
+                        style = Stroke(10f)
+
+                    )
+                }
+            }
+
 
 
         }
